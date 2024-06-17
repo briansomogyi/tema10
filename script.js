30df92fbf32490d2fb829d62c9b2c5e119e44a3e
briansomogyi <brian.somogyi@emanuel.ro>
2024-06-17 07:45:28 +0300

    Ceva nu a mers, schimb branch-ul

--- script.js
+++ script.js
@@ -11,43 +11,25 @@ class Stair {
     }
   }
   
-  class FractalStairs {
-    constructor(x, y, width, height, levels) {
-      this.x = x;
-      this.y = y;
-      this.width = width;
-      this.height = height;
-      this.levels = levels;
-      this.stairs = [];
-      this.createStairs();
+  function drawFractalStairs(x, y, width, height, levels) {
+    if (levels === 0) {
+      return;
     }
-  
-    createStairs() {
-      let w = this.width;
-      let h = this.height;
-      for (let i = 0; i < this.levels; i++) {
-        this.stairs.push(new Stair(this.x + i * w, this.y - i * h, w, h));
-        w *= 0.5;
-        h *= 0.5;
-      }
-    }
-  
-    display() {
-      for (let stair of this.stairs) {
-        stair.display();
-      }
-    }
+    
+    let stair = new Stair(x, y, width, height);
+    stair.display();
+    
+    drawFractalStairs(x + width / 2, y - height / 2, width / 2, height / 2, levels - 1);
   }
-  
-  let fractalStairs;
   
   function setup() {
     createCanvas(400, 400);
-    fractalStairs = new FractalStairs(50, height - 50, 80, 20, 5);
   }
   
   function draw() {
     background(220);
-    fractalStairs.display();
+    
+    let levels = 5; // You can change the number of levels here
+    drawFractalStairs(50, height - 50, 80, 20, levels);
   }
   
